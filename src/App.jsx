da7bf0cd6213f4de9d3c25fe955a3e1792595800
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-06-18 21:49:10 +0530

    implemented the stripe component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Work from "./components/Work";
 import Home from "./components/Home";
 import Culture from "./components/Culture";
+import Stripes from "./components/Stripes";
 
 function App() {
   return (
     <div className="w-full h-screen bg-zinc-900 text-white font-['satoshi']">
       <Navbar />
       <Work />
+      <Stripes />
     </div>
   );
 }
