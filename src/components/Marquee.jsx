0928fa1b2611ecda8ea1b86b127826279bf3f318
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-06-23 18:14:11 +0530

    implemented the animation to marquee component

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,14 +1,30 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-function Marquee({ imagesUrl }) {
+function Marquee({ imagesUrl, direction }) {
   return (
-    <div className="flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden">
-      {imagesUrl.map((url, index) => (
-        <img key={index} src={url} className="w-30 flex-shrink-0" />
-      ))}
-      {imagesUrl.map((url, index) => (
-        <img key={index} src={url} className="" />
-      ))}
+    <div className="flex w-full overflow-hidden">
+      <motion.div
+        initial={{ x: direction === "left" ? "0" : "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 15, repeat: Infinity }}
+        className="flex flex-shrink-0 gap-40 py-10 pr-40"
+      >
+        {imagesUrl.map((url, index) => (
+          <img key={index} src={url} className="" />
+        ))}
+      </motion.div>
+
+      <motion.div
+        initial={{ x: direction === "left" ? "0" : "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 15, repeat: Infinity }}
+        className="flex flex-shrink-0 gap-40 py-10 pr-40"
+      >
+        {imagesUrl.map((url, index) => (
+          <img key={index} src={url} className="" />
+        ))}
+      </motion.div>
     </div>
   );
 }
